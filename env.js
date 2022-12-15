52a93db272758bab3278c91c3dab035c5795e8d9
Moexin <moexin@qq.com>
2022-12-16 04:16:53 +0800

    V1.7.1

--- env.js
+++ env.js
@@ -1,1 +1,23 @@
-window.settings={title:'Hash Cloud',description:'IPLC专线机场',host:'https://api.hashyun.top',theme:{sidebar:'light',header:'dark',color:'default'},i18n:['zh-CN','en-US','ja-JP','vi-VN','ko-KR','zh-TW']}
+window.settings = {
+  // 站点标题
+  title: 'Hash Cloud',
+  // 站点描述
+  description: 'IPLC专线机场',
+  // API
+  host: 'https://api.hashyun.top',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
